693d0da23ccf37b714fc9caa7d2add5b5750ec5b
Yash <155367011+rehaneditzzz@users.noreply.github.com>
2025-02-16 11:23:41 +0530

    Update SignupForm.jsx

--- Frontend/src/Auth/SignupForm.jsx
+++ Frontend/src/Auth/SignupForm.jsx
@@ -36,7 +36,7 @@ const SignupForm = () => {
     setServerError("");
     try {
       const response = await axios.post(
-        "http://localhost:3000/api/auth/signup",
+        "https://new-web-complete-api.vercel.app/api/auth/signup",
         data,
         {
           withCredentials: true,
